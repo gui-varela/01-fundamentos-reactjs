81643f12f0c707f25f45bb45de71135d9e3648af
Guilherme Varela <guilhermevareladossantos@gmail.com>
2023-04-30 21:14:10 -0300

    Aula 1.2.9 - Component: Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,39 @@
+import { Trash, ThumbsUp } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/gui-varela.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Guilherme Varela</strong>
+              <time 
+                title='11 de maio às 01:02' 
+                dateTime='2023-04-08 01:02:25'
+              >
+                Há cerca de 1h
+              </time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
