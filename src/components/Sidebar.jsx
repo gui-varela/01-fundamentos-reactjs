66de041978b11bbe8b82577812ecfc80d92e15b2
Guilherme Varela <guilhermevareladossantos@gmail.com>
2023-05-01 02:13:25 -0300

    Aula 1.2.11 - Componente: Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,12 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <img 
-        className={styles.cover} 
+      <Avatar
         src="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
       />
     
